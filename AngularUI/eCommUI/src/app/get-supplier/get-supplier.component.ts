da86f17c61101dc4fcda5f9344afb0f672a915c6
Aravind <aravindgoudmarka1@gmail.com>
2024-08-13 21:34:13 +0530

    Supplier Changes

--- AngularUI/eCommUI/src/app/get-supplier/get-supplier.component.ts
+++ AngularUI/eCommUI/src/app/get-supplier/get-supplier.component.ts
@@ -1,23 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SupplierService } from '../services/supplier.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTrash,faEdit, faL } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-get-supplier',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './get-supplier.component.html',
   styleUrl: './get-supplier.component.css'
 })
-export class GetSupplierComponent {
+export class GetSupplierComponent implements OnInit{
   constructor(private supplierService : SupplierService){}
-
+  faTrash = faTrash;
+  faEdit = faEdit;
   suppliers: any[]=[];
   responseMsg: any={};
+  showGrid:boolean=false;
+  public ngOnInit(): void
+  {
+    this.getSuppliers();
+  }
 
   getSuppliers(){
     this.supplierService.getsupplier().subscribe(data => {
       this.suppliers = data as any[];
-      console.log("custResponse",this.suppliers);
+      this.showGrid=true;
     });
   }
   insertSupplier(){
